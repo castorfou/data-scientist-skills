0a71661b83660a451fb81c4fafbe5769b40922ec
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-07-19 13:39:11 -0400

    1er tests avec numpy

---
+++ python-sandbox/1st project/numpy_tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 13:37:22 2019
+
+@author: N561507
+
+qqes tests sur numpy
+"""
+
+import numpy as np
+
+TRANSPORT = ['train','avion']
+np_transport = np.array(TRANSPORT)
