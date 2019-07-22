13934deefb72bdfc79f445f35cf11aefacfdc1b2
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-07-22 14:21:57 -0400

    end of linear regression

--- python-sandbox/introduction-to-tensorflow-in-python/chapiter2.py
+++ python-sandbox/introduction-to-tensorflow-in-python/chapiter2.py
@@ -231,9 +231,34 @@ plot_results(intercept, slope)
 #Select from the menu Tools > Preferences, then IPython console in the list of categories on the left, then the tab Graphics at the top, and change the Graphics backend from Inline to e.g. Automatic
 
 
+#%% Multiple linear regression
+bedrooms=housing.bedrooms.to_numpy()
+print(max(bedrooms))
+params=Variable([0.1 , 0.05, 0.02], dtype=tf.float32)
+
+# Define the linear regression model
+def linear_regression(params, feature1 = size_log, feature2 = bedrooms):
+	return params[0] + feature1*params[1] + feature2*params[2]
+
+# Define the loss function
+def loss_function(params, targets = price_log, feature1 = size_log, feature2 = bedrooms):
+	# Set the predicted values
+	predictions = linear_regression(params, feature1, feature2)  
+  
+	# Use the mean absolute error loss
+	return keras.losses.mae(targets, predictions)
+
+# Define the optimize operation
+opt = keras.optimizers.Adam()
+
+def print_results(params):
+	return print('loss: {:0.3f}, intercept: {:0.3f}, slope_1: {:0.3f}, slope_2: {:0.3f}'.format(loss_function(params).numpy(), params[0].numpy(), params[1].numpy(), params[2].numpy()))
 
 
-
+# Perform minimization and print trainable variables
+for j in range(10):
+	opt.minimize(lambda: loss_function(params), var_list=[params])
+	print_results(params)
 
 
 
