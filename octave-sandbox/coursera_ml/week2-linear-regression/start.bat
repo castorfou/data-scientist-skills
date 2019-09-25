d36ecd5df3b7787b90a351fe1c1ba06e334dbf4c
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-09-25 14:48:00 +0200

    end of week

---
+++ octave-sandbox/coursera_ml/week2-linear-regression/start.bat
@@ -0,0 +1,4 @@
+@echo on
+d:
+cd "D:\git\datacamp-itp\data-scientist-skills\octave-sandbox\coursera_ml\week2-linear-regression\machine-learning-ex1\ex1"
+%SYSTEMROOT%\system32\wscript.exe "C:\Octave\Octave-5.1.0.0\octave.vbs" --force-gui
---
+++ octave-sandbox/coursera_ml/week3-logistic-regression/machine-learning-ex2/ex2/costFunctionReg.m
@@ -0,0 +1,34 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+theta_without1stItem=theta;
+theta_without1stItem(1)=0;
+
+J = sum( -y .* log(sigmoid(X*theta))-(1-y) .* log(1-sigmoid(X*theta)) )/ m + lambda*sum(theta_without1stItem.^2)/ (2*m);
+
+grad(1)=sum( (sigmoid(X*theta)-y) .* X(:,1)) / m;
+
+for iter2 = 2:length(theta)
+      grad(iter2)= (sum( (sigmoid(X*theta)-y) .* X(:,iter2)) +lambda*theta(iter2))/ m;
+endfor
+
+
+
+% =============================================================
+
+end
