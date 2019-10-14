b3b5bc5bb55fcc77218d7f695daeed5335277a52
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-10-14 17:08:57 +0200

    coursera week 4 - neural network representation

---
+++ octave-sandbox/coursera_ml/week4-neural-network-representation/start octave env.bat
@@ -0,0 +1,4 @@
+@echo on
+d:
+cd "D:\git\datacamp-itp\data-scientist-skills\octave-sandbox\coursera_ml\week4-neural-network-representation\machine-learning-ex3\ex3"
+%SYSTEMROOT%\system32\wscript.exe "C:\Octave\Octave-5.1.0.0\octave.vbs" --force-gui
