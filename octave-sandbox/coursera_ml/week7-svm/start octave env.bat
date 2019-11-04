9faddd009f3d041103f7846ba9e53d000f455224
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-11-04 17:59:23 +0100

    coursera SVM

---
+++ octave-sandbox/coursera_ml/week7-svm/start octave env.bat
@@ -0,0 +1,4 @@
+@echo on
+d:
+cd "D:\git\datacamp-itp\data-scientist-skills\octave-sandbox\coursera_ml\week7-svm\machine-learning-ex6\ex6"
+%SYSTEMROOT%\system32\wscript.exe "C:\Octave\Octave-5.1.0.0\octave.vbs" --force-gui
