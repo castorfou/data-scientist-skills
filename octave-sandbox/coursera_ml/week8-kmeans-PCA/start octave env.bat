7986e775beecf72233299448f6266b9fcd178237
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-11-12 15:35:33 +0100

    PCA and k-means

---
+++ octave-sandbox/coursera_ml/week8-kmeans-PCA/start octave env.bat
@@ -0,0 +1,4 @@
+@echo on
+d:
+cd "D:\git\datacamp-itp\data-scientist-skills\octave-sandbox\coursera_ml\week8-kmeans-PCA\machine-learning-ex7\ex7"
+%SYSTEMROOT%\system32\wscript.exe "C:\Octave\Octave-5.1.0.0\octave.vbs" --force-gui
