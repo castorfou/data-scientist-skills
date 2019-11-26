1134671819dbd177d313d8110d01d20c8dafc10e
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2019-11-26 08:03:50 +0100

    update des plots avec des points "cercles" noirs

--- python-sandbox/zoe/2 - start_env.bat
+++ python-sandbox/zoe/2 - start_env.bat
@@ -2,6 +2,6 @@
 set Path=C:\Users\F279814\AppData\Local\Continuum\anaconda3;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\mingw-w64\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\usr\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Scripts;C:\Users\F279814\AppData\Local\Continuum\anaconda3\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\condabin;%PATH%
 REM open conda command prompt
 d:
-cd D:\git\datacamp-itp\data-scientist-skills\python-sandbox\zoe
+cd D:\git\data-scientist-skills\python-sandbox\zoe
 call conda activate zoe 
-jupyter notebook
+jupyter notebook zoe_prevision-37-mois.ipynb
