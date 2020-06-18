13e7eb656580b67b6e889f21b757977e7a5b9920
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2020-06-18 09:53:38 +0200

    violin 2 faces

--- python-sandbox/gaetan-boxplot/exports_py/data viz combo chart.py
+++ python-sandbox/gaetan-boxplot/exports_py/data viz combo chart.py
@@ -13,7 +13,7 @@
 
 # ## chargement des données
 
-# In[34]:
+# In[1]:
 
 
 import pandas as pd
@@ -23,25 +23,25 @@ filename = 'DATASET_ML25307.xlsx'
 df_ml25307 = pd.read_excel(filename, parse_dates=[['Date', 'Heure']])
 
 
-# In[35]:
+# In[2]:
 
 
 df_ml25307.info()
 
 
-# In[36]:
+# In[3]:
 
 
 df_ml25307.Date_Heure.min(), df_ml25307.Date_Heure.max(), 
 
 
-# In[37]:
+# In[4]:
 
 
 df_ml25307['SITE Z'].value_counts()
 
 
-# In[38]:
+# In[5]:
 
 
 import matplotlib.pyplot as plt
@@ -53,7 +53,7 @@ plt.show()
 
 # ### jour Michelin 
 
-# In[39]:
+# In[6]:
 
 
 def ajoute_un_jour_Michelin(date, heureEquipeA):
@@ -71,13 +71,13 @@ def dateMichelin_vers_dateCalendaire(df, heureEquipeA = 6):
     return df_cop
 
 
-# In[40]:
+# In[7]:
 
 
 df_ml25307_date_calendaire = dateMichelin_vers_dateCalendaire(df_ml25307, heureEquipeA=6)
 
 
-# In[41]:
+# In[8]:
 
 
 fig, ax = plt.subplots(figsize=(15,7))
@@ -87,7 +87,7 @@ plt.show()
 
 # ### split date et heure
 
-# In[42]:
+# In[9]:
 
 
 df_ml25307_date_calendaire['Date'] = pd.to_datetime(df_ml25307_date_calendaire['Date_Heure']).dt.date
@@ -95,14 +95,14 @@ df_ml25307_date_calendaire['Time'] = pd.to_datetime(df_ml25307_date_calendaire['
 df_ml25307_date_calendaire['Date'] = pd.to_datetime(df_ml25307_date_calendaire['Date'])
 
 
-# In[43]:
+# In[10]:
 
 
 df_ml25307_date_calendaire.sort_values(by='Date_Heure', axis=0, ascending=True, inplace=True)
 df_ml25307_date_calendaire.reset_index(inplace=True)
 
 
-# In[44]:
+# In[11]:
 
 
 df_ml25307_date_calendaire
@@ -110,7 +110,7 @@ df_ml25307_date_calendaire
 
 # ### trim classement
 
-# In[45]:
+# In[12]:
 
 
 df_ml25307_date_calendaire['Clt'] = df_ml25307_date_calendaire['Clt'].str.strip()
@@ -120,7 +120,7 @@ df_ml25307_date_calendaire['Clt'] = df_ml25307_date_calendaire['Clt'].str.strip(
 
 # ### boxplot + swarmplot
 
-# In[46]:
+# In[13]:
 
 
 import seaborn as sns
@@ -142,7 +142,7 @@ g2.set_xticklabels(xlabels, rotation=30);
 
 # ### violinplot + swarmplot
 
-# In[47]:
+# In[14]:
 
 
 import seaborn as sns
@@ -165,7 +165,7 @@ g2.set_xticklabels(xlabels, rotation=30);
 
 # ### violinplot with 2 sides
 
-# In[53]:
+# In[16]:
 
 
 import seaborn as sns
@@ -179,11 +179,61 @@ sns.set(style="ticks", palette="pastel")
 fig_dims = (17, 8)
 fig, ax = plt.subplots(figsize=fig_dims)
 #hue_order=['D', 'C', 'P 5', 'P 10', 'P 15', 'H'],
-g1 = sns.violinplot(x="Date", y="TEMCHIMI", hue="Clt", split=True, inner="quart",data=df_sous_ensemble_DC, ax=ax)
+g1 = sns.violinplot(x="Date", y="TEMCHIMI", hue="Clt", split=True, inner="quart",data=df_sous_ensemble_DC, ax=ax, palette='muted')
 g2 = sns.swarmplot(x="Date", y="TEMCHIMI", hue="Clt", data=df_sous_ensemble_DC, ax=ax)
 xlabels =[pd.to_datetime(str(x)).strftime("%d-%m") for x in set(df_sous_ensemble_DC['Date'].values)]
 g1.set_xticklabels(xlabels, rotation=30)
 g2.set_xticklabels(xlabels, rotation=30);
+
+
+# ### violinplot + standard plot of TEMCHIMI
+
+# In[20]:
+
+
+import seaborn as sns
+
+
+df_sous_ensemble = df_ml25307_date_calendaire[df_ml25307_date_calendaire.Date >= '2019-09-10']
+df_sous_ensemble_DC = df_sous_ensemble[df_sous_ensemble.Clt.isin(['D', 'C'])]
+
+sns.set(style="ticks", palette="pastel")
+
+fig_dims = (17, 8)
+fig, ax = plt.subplots(figsize=fig_dims)
+#hue_order=['D', 'C', 'P 5', 'P 10', 'P 15', 'H'],
+g1 = sns.violinplot(x="Date", y="TEMCHIMI", hue="Clt", split=True, inner="quart",data=df_sous_ensemble_DC, ax=ax, palette='muted')
+g2 = sns.swarmplot(x="Date", y="TEMCHIMI", hue="Clt", data=df_sous_ensemble_DC, ax=ax)
+xlabels =[pd.to_datetime(str(x)).strftime("%d-%m") for x in set(df_sous_ensemble_DC['Date'].values)]
+g1.set_xticklabels(xlabels, rotation=30)
+g2.set_xticklabels(xlabels, rotation=30);
+
+ax2 = ax.twinx()
+sns.lineplot(x="Date_Heure", y="TEMCHIMI", ax=ax2, data=df_sous_ensemble_DC)
+plt.show()
+
+
+# In[30]:
+
+
+import seaborn as sns
+
+
+df_sous_ensemble = df_ml25307_date_calendaire[df_ml25307_date_calendaire.Date >= '2019-09-10']
+df_sous_ensemble_DC = df_sous_ensemble[df_sous_ensemble.Clt.isin(['D', 'C'])]
+
+sns.set(style="ticks", palette="pastel")
+
+fig_dims = (17, 8)
+fig, ax = plt.subplots(figsize=fig_dims)
+
+sns.lineplot(x="Date_Heure", y="TEMCHIMI", ax=ax, data=df_sous_ensemble_DC)
+ax2 = ax.twinx()
+g1 = sns.violinplot(x="Date", y="TEMCHIMI", hue="Clt", split=True, inner="quart",data=df_sous_ensemble_DC, ax=ax2, palette='muted')
+xlabels =[pd.to_datetime(str(x)).strftime("%d-%m") for x in set(df_sous_ensemble_DC['Date'].values)]
+g1.set_xticklabels(xlabels, rotation=30)
+
+plt.show()
 
 
 # In[ ]:
