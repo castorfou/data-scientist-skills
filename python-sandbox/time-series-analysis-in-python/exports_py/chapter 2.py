74e9848126800438f123ce65a9bda606f6ccd719
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2020-06-26 11:39:09 +0200

    timeseries mi chap 2

--- python-sandbox/time-series-analysis-in-python/exports_py/chapter 2.py
+++ python-sandbox/time-series-analysis-in-python/exports_py/chapter 2.py
@@ -4,6 +4,127 @@
 # # Autocorrelation Function
 # 
 
+# ## Taxing Exercise: Compute the ACF
+# In the last chapter, you computed autocorrelations with one lag. Often we are interested in seeing the autocorrelation over many lags. The quarterly earnings for H&R Block (ticker symbol HRB) is plotted on the right, and you can see the extreme cyclicality of its earnings. A vast majority of its earnings occurs in the quarter that taxes are due.
+# 
+# You will compute the array of autocorrelations for the H&R Block quarterly earnings that is pre-loaded in the DataFrame HRB. Then, plot the autocorrelation function using the plot_acf module. This plot shows what the autocorrelation function looks like for cyclical earnings data. The ACF at lag=0 is always one, of course. In the next exercise, you will learn about the confidence interval for the ACF, but for now, suppress the confidence interval by setting alpha=1.
+
+# ### init
+
+# In[1]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(HRB)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'HRB.csv': 'https://file.io/cpCIahVU'}}
+"""
+prefixToc='1.1'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="10.225.92.1:80")
+
+#initialisation
+
+import pandas as pd
+HRB = pd.read_csv(prefix+'HRB.csv',index_col=0)
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+HRB.plot()
+
+
+# ### code
+
+# In[6]:
+
+
+# Import the acf module and the plot_acf module from statsmodels
+from statsmodels.tsa.stattools import acf
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Compute the acf array of HRB
+acf_array = acf(HRB)
+print(acf_array)
+
+# Plot the acf function
+plot_acf(HRB, alpha=1)
+plt.show()
+
+
+# ## Are We Confident This Stock is Mean Reverting?
+# In the last chapter, you saw that the autocorrelation of MSFT's weekly stock returns was -0.16. That autocorrelation seems large, but is it statistically significant? In other words, can you say that there is less than a 5% chance that we would observe such a large negative autocorrelation if the true autocorrelation were really zero? And are there any autocorrelations at other lags that are significantly different from zero?
+# 
+# Even if the true autocorrelations were zero at all lags, in a finite sample of returns you won't see the estimate of the autocorrelations exactly zero. In fact, the standard deviation of the sample autocorrelation is 1/N−−√ where N is the number of observations, so if N=100, for example, the standard deviation of the ACF is 0.1, and since 95% of a normal curve is between +1.96 and -1.96 standard deviations from the mean, the 95% confidence interval is ±1.96/N−−√. This approximation only holds when the true autocorrelations are all zero.
+# 
+# You will compute the actual and approximate confidence interval for the ACF, and compare it to the lag-one autocorrelation of -0.16 from the last chapter. The weekly returns of Microsoft is pre-loaded in a DataFrame called returns.
+
+# ### init
+
+# In[7]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(returns)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'returns.csv': 'https://file.io/v1zD5NBl'}}
+"""
+prefixToc='1.2'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="10.225.92.1:80")
+
+#initialisation
+
+import pandas as pd
+returns = pd.read_csv(prefix+'returns.csv',index_col=0)
+
+
+# ### code
+
+# In[11]:
+
+
+# Import the plot_acf module from statsmodels and sqrt from math
+from statsmodels.graphics.tsaplots import plot_acf
+from math import sqrt
+
+# Compute and print the autocorrelation of MSFT weekly returns
+autocorrelation = returns['Adj Close'].autocorr()
+print("The autocorrelation of weekly MSFT returns is %4.2f" %(autocorrelation))
+
+# Find the number of observations by taking the length of the returns DataFrame
+nobs = len(returns)
+
+# Compute the approximate confidence interval
+conf = 1.96/sqrt(nobs)
+print("The approximate confidence interval is +/- %4.2f" %(conf))
+
+# Plot the autocorrelation function with 95% confidence intervals and 20 lags using plot_acf
+plot_acf(returns, alpha=0.05, lags=20)
+plt.show()
+
+
+# # White Noise
+# 
+
 # In[ ]:
 
 
