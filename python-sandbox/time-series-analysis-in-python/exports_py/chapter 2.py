365d272ad88f9ebb57892a4d644afc99bc8aa882
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2020-06-26 16:32:53 +0200

    timeseries

--- python-sandbox/time-series-analysis-in-python/exports_py/chapter 2.py
+++ python-sandbox/time-series-analysis-in-python/exports_py/chapter 2.py
@@ -125,6 +125,183 @@ plt.show()
 # # White Noise
 # 
 
+# ## Can't Forecast White Noise
+# A white noise time series is simply a sequence of uncorrelated random variables that are identically distributed. Stock returns are often modeled as white noise. Unfortunately, for white noise, we cannot forecast future observations based on the past - autocorrelations at all lags are zero.
+# 
+# You will generate a white noise series and plot the autocorrelation function to show that it is zero for all lags. You can use np.random.normal() to generate random returns. For a Gaussian white noise process, the mean and standard deviation describe the entire process.
+# 
+# Plot this white noise series to see what it looks like, and then plot the autocorrelation function.
+
+# ### code
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[4]:
+
+
+# Import the plot_acf module from statsmodels
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Simulate white noise returns
+returns = np.random.normal(loc=0.02, scale=0.05, size=1000)
+
+# Print out the mean and standard deviation of returns
+mean = np.mean(returns)
+std = np.std(returns)
+print("The mean is %5.3f and the standard deviation is %5.3f" %(mean,std))
+
+# Plot returns series
+plt.plot(returns)
+plt.show()
+
+# Plot autocorrelation function of white noise returns
+plot_acf(returns, lags=20)
+plt.show()
+
+
+# # Random Walk
+# 
+
+# ## Generate a Random Walk
+# Whereas stock returns are often modeled as white noise, stock prices closely follow a random walk. In other words, today's price is yesterday's price plus some random noise.
+# 
+# You will simulate the price of a stock over time that has a starting price of 100 and every day goes up or down by a random amount. Then, plot the simulated stock price. If you hit the "Run Code" code button multiple times, you'll see several realizations.
+
+# ### code
+
+# In[7]:
+
+
+# Generate 500 random steps with mean=0 and standard deviation=1
+steps = np.random.normal(loc=0, scale=1, size=500)
+
+# Set first element to 0 so that the first price will be the starting stock price
+steps[0]=0
+
+# Simulate stock prices, P with a starting price of 100
+P = 100 + np.cumsum(steps)
+
+# Plot the simulated stock prices
+plt.plot(P)
+plt.title("Simulated Random Walk")
+plt.show()
+
+
+# ## Get the Drift
+# In the last exercise, you simulated stock prices that follow a random walk. You will extend this in two ways in this exercise.
+# 
+# You will look at a random walk with a drift. Many time series, like stock prices, are random walks but tend to drift up over time.
+# In the last exercise, the noise in the random walk was additive: random, normal changes in price were added to the last price. However, when adding noise, you could theoretically get negative prices. Now you will make the noise multiplicative: you will add one to the random, normal changes to get a total return, and multiply that by the last price.
+
+# ### code
+
+# In[9]:
+
+
+# Generate 500 random steps
+steps = np.random.normal(loc=0.001, scale=0.01, size=500) + 1
+
+# Set first element to 1
+steps[0]=1
+
+# Simulate the stock price, P, by taking the cumulative product
+P = 100 * np.cumprod(steps)
+
+# Plot the simulated stock prices
+plt.plot(P)
+plt.title("Simulated Random Walk with Drift")
+plt.show()
+
+
+# ## Are Stock Prices a Random Walk?
+# Most stock prices follow a random walk (perhaps with a drift). You will look at a time series of Amazon stock prices, pre-loaded in the DataFrame AMZN, and run the 'Augmented Dickey-Fuller Test' from the statsmodels library to show that it does indeed follow a random walk.
+# 
+# With the ADF test, the "null hypothesis" (the hypothesis that we either reject or fail to reject) is that the series follows a random walk. Therefore, a low p-value (say less than 5%) means we can reject the null hypothesis that the series is a random walk.
+
+# ### init
+
+# In[10]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(AMZN)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'AMZN.csv': 'https://file.io/FFQzHDcK'}}
+"""
+prefixToc='3.3'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="10.225.92.1:80")
+
+#initialisation
+
+import pandas as pd
+AMZN = pd.read_csv(prefix+'AMZN.csv',index_col=0)
+
+
+# ### code
+
+# In[11]:
+
+
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Run the ADF test on the price series and print out the results
+results = adfuller(AMZN)
+print(results)
+
+# Just print out the p-value
+print('The p-value of the test on prices is: ' + str(results[1]))
+
+
+# ![image.png](attachment:image.png)
+
+# 
+# ## How About Stock Returns?
+# In the last exercise, you showed that Amazon stock prices, contained in the DataFrame AMZN follow a random walk. In this exercise. you will do the same thing for Amazon returns (percent change in prices) and show that the returns do not follow a random walk.
+
+# ### code
+
+# In[12]:
+
+
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Create a DataFrame of AMZN returns
+AMZN_ret = AMZN.pct_change()
+
+# Eliminate the NaN in the first row of returns
+AMZN_ret = AMZN_ret.dropna()
+
+# Run the ADF test on the return series and print out the p-value
+results = adfuller(AMZN_ret)
+print('The p-value of the test on returns is: ' + str(results[1]))
+
+
+# ![image.png](attachment:image.png)
+
+# # Stationarity
+
+# ## Seasonal Adjustment During Tax Season
+# Many time series exhibit strong seasonal behavior. The procedure for removing the seasonal component of a time series is called seasonal adjustment. For example, most economic data published by the government is seasonally adjusted.
+# 
+# You saw earlier that by taking first differences of a random walk, you get a stationary white noise process. For seasonal adjustments, instead of taking first differences, you will take differences with a lag corresponding to the periodicity.
+# 
+# Look again at the ACF of H&R Block's quarterly earnings, pre-loaded in the DataFrame HRB, and there is a clear seasonal component. The autocorrelation is high for lags 4,8,12,16,… because of the spike in earnings every four quarters during tax season. Apply a seasonal adjustment by taking the fourth difference (four represents the periodicity of the series). Then compute the autocorrelation of the transformed series.
+
 # In[ ]:
 
 
