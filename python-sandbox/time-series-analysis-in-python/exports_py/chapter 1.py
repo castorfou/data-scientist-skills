e722b1ccffaed9984c25f2e3346b5b215bab85ee
Guillaume Ramelet <guillaume.ramelet@michelin.com>
2020-06-26 10:22:46 +0200

    debut time series analysis

---
+++ python-sandbox/time-series-analysis-in-python/exports_py/chapter 1.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction to Course
+# 
+
+# ## A "Thin" Application of Time Series
+# Google Trends allows users to see how often a term is searched for. We downloaded a file from Google Trends containing the frequency over time for the search word "diet", which is pre-loaded in a DataFrame called diet. A first step when analyzing a time series is to visualize the data with a plot. You should be able to clearly see a gradual decrease in searches for "diet" throughout the calendar year, hitting a low around the December holidays, followed by a spike in searches around the new year as people make New Year's resolutions to lose weight.
+# 
+# Like many time series datasets you will be working with, the index of dates are strings and should be converted to a datetime index before plotting.
+# 
+# This course touches on a lot of concepts you may have forgotten, so if you ever need a quick refresher, download the pandas basics Cheat Sheet and keep it handy!
+
+# ### init
+
+# In[1]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(diet)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'diet.csv': 'https://file.io/2HBNyrYO'}}
+"""
+prefixToc='1.1'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+diet = pd.read_csv(prefix+'diet.csv',index_col=0)
+
+
+# ### code
+# 
+
+# In[2]:
+
+
+# Import pandas and plotting modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Convert the date index to datetime
+diet.index = pd.to_datetime(diet.index)
+
+
+# In[3]:
+
+
+# Plot the entire time series diet and show gridlines
+diet.plot(grid=True)
+plt.show()
+
+
+# In[5]:
+
+
+# Slice the dataset to keep only 2012
+diet2012 = diet['2012']
+
+# Plot 2012 data
+diet2012.plot(grid=True)
+plt.show()
+
+
+# ## Merging Time Series With Different Dates
+# Stock and bond markets in the U.S. are closed on different days. For example, although the bond market is closed on Columbus Day (around Oct 12) and Veterans Day (around Nov 11), the stock market is open on those days. One way to see the dates that the stock market is open and the bond market is closed is to convert both indexes of dates into sets and take the difference in sets.
+# 
+# The pandas .join() method is a convenient tool to merge the stock and bond DataFrames on dates when both markets are open.
+# 
+# Stock prices and 10-year US Government bond yields, which were downloaded from FRED, are pre-loaded in DataFrames stocks and bonds.
+
+# ### init
+
+# In[1]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(stocks, bonds)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'bonds.csv': 'https://file.io/qvGVkTtG',
+  'stocks.csv': 'https://file.io/sdpNa3SO'}}
+  """
+prefixToc='1.2'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+stocks = pd.read_csv(prefix+'stocks.csv',index_col=0)
+bonds = pd.read_csv(prefix+'bonds.csv',index_col=0)
+
+
+# ### code
+
+# In[3]:
+
+
+# Import pandas
+import pandas as pd
+
+# Convert the stock index and bond index into sets
+set_stock_dates = set(stocks.index)
+set_bond_dates = set(bonds.index)
+
+# Take the difference between the sets and print
+print(set_stock_dates - set_bond_dates)
+
+# Merge stocks and bonds DataFrames using join()
+stocks_and_bonds = stocks.join(bonds, how='inner')
+
+
+# # Correlation of Two Time Series
+# 
+
+# ## Correlation of Stocks and Bonds
+# Investors are often interested in the correlation between the returns of two different assets for asset allocation and hedging purposes. In this exercise, you'll try to answer the question of whether stocks are positively or negatively correlated with bonds. Scatter plots are also useful for visualizing the correlation between the two variables.
+# 
+# Keep in mind that you should compute the correlations on the percentage changes rather than the levels.
+# 
+# Stock prices and 10-year bond yields are combined in a DataFrame called stocks_and_bonds under columns SP500 and US10Y
+# 
+# The pandas and plotting modules have already been imported for you. For the remainder of the course, pandas is imported as pd and matplotlib.pyplot is imported as plt.
+
+# ### code
+
+# In[6]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[8]:
+
+
+# Compute percent change using pct_change()
+returns = stocks_and_bonds.pct_change()
+
+# Compute correlation using corr()
+correlation = returns['SP500'].corr(returns['US10Y'])
+print("Correlation of stocks and interest rates: ", correlation)
+
+# Make scatter plot
+plt.scatter(returns['SP500'], returns['US10Y'])
+plt.show()
+
+
+# ## Flying Saucers Aren't Correlated to Flying Markets
+# Two trending series may show a strong correlation even if they are completely unrelated. This is referred to as "spurious correlation". That's why when you look at the correlation of say, two stocks, you should look at the correlation of their returns and not their levels.
+# 
+# To illustrate this point, calculate the correlation between the levels of the stock market and the annual sightings of UFOs. Both of those time series have trended up over the last several decades, and the correlation of their levels is very high. Then calculate the correlation of their percent changes. This will be close to zero, since there is no relationship between those two series.
+# 
+# The DataFrame levels contains the levels of DJI and UFO. UFO data was downloaded from www.nuforc.org.
+
+# ### init
+
+# In[9]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(levels)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'levels.csv': 'https://file.io/LsSwWQOe'}}
+"""
+prefixToc='2.2'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+levels = pd.read_csv(prefix+'levels.csv',index_col=0)
+
+
+# ### code
+
+# In[12]:
+
+
+# Compute correlation of levels
+correlation1 = levels['DJI'].corr(levels['UFO'])
+print("Correlation of levels: ", correlation1)
+
+# Compute correlation of percent changes
+changes = levels.pct_change()
+correlation2 = changes['DJI'].corr(changes['UFO'])
+print("Correlation of changes: ", correlation2)
+
+
+# In[13]:
+
+
+levels.plot()
+
+
+# In[14]:
+
+
+changes.plot()
+
+
+# # Simple Linear Regression
+# 
+
+# ## Looking at a Regression's R-Squared
+# R-squared measures how closely the data fit the regression line, so the R-squared in a simple regression is related to the correlation between the two variables. In particular, the magnitude of the correlation is the square root of the R-squared and the sign of the correlation is the sign of the regression coefficient.
+# 
+# In this exercise, you will start using the statistical package statsmodels, which performs much of the statistical modeling and testing that is found in R and software packages like SAS and MATLAB.
+# 
+# You will take two series, x and y, compute their correlation, and then regress y on x using the function OLS(y,x) in the statsmodels.api library (note that the dependent, or right-hand side variable y is the first argument). Most linear regressions contain a constant term which is the intercept (the α in the regression yt=α+βxt+ϵt). To include a constant using the function OLS(), you need to add a column of 1's to the right hand side of the regression.
+# 
+# The module statsmodels.api has been imported for you as sm.
+
+# ### init
+
+# In[15]:
+
+
+
+###################
+##### pandas Serie
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(x, y)
+"""
+
+tobedownloaded="""
+{pandas.core.series.Series: {'x.csv': 'https://file.io/PTynMfvG',
+  'y.csv': 'https://file.io/U9iSpyQd'}}
+"""
+prefixToc='3.1'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+y = pd.read_csv(prefix+'y.csv',index_col=0, header=None,squeeze=True)
+x = pd.read_csv(prefix+'x.csv',index_col=0, header=None,squeeze=True)
+
+
+# ### code
+
+# In[34]:
+
+
+# Import the statsmodels module
+import statsmodels.api as sm
+
+# Compute correlation of x and y
+correlation = x.corr(y)
+print("The correlation between x and y is %4.2f" %(correlation))
+
+# Convert the Series x to a DataFrame and name the column x
+dfx = pd.DataFrame(x)
+# Add a constant to the DataFrame dfx
+dfx1 = sm.add_constant(dfx)
+
+# Regress y on dfx1
+result = sm.OLS(y, dfx1).fit()
+
+# Print out the results and look at the relationship between R-squared and the correlation above
+print(result.summary())
+
+
+# # Autocorrelation
+
+# ## A Popular Strategy Using Autocorrelation
+# One puzzling anomaly with stocks is that investors tend to overreact to news. Following large jumps, either up or down, stock prices tend to reverse. This is described as mean reversion in stock prices: prices tend to bounce back, or revert, towards previous levels after large moves, which are observed over time horizons of about a week. A more mathematical way to describe mean reversion is to say that stock returns are negatively autocorrelated.
+# 
+# This simple idea is actually the basis for a popular hedge fund strategy. If you're curious to learn more about this hedge fund strategy (although it's not necessary reading for anything else later in the course), see here.
+# 
+# You'll look at the autocorrelation of weekly returns of MSFT stock from 2012 to 2017. You'll start with a DataFrame MSFT of daily prices. You should use the .resample() method to get weekly prices and then compute returns from prices. Use the pandas method .autocorr() to get the autocorrelation and show that the autocorrelation is negative. Note that the .autocorr() method only works on Series, not DataFrames (even DataFrames with one column), so you will have to select the column in the DataFrame.
+
+# ### init
+
+# In[42]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(MSFT)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'MSFT.csv': 'https://file.io/365JqDdm'}}
+"""
+prefixToc='4.1'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+MSFT = pd.read_csv(prefix+'MSFT.csv',index_col=0)
+MSFT.index = pd.to_datetime(MSFT.index)
+
+
+# ### code
+
+# In[46]:
+
+
+# Convert the daily data to weekly data
+MSFT = MSFT.resample(rule='W', how='last')
+
+# Compute the percentage change of prices
+returns = MSFT.pct_change()
+
+# Compute and print the autocorrelation of returns
+autocorrelation = returns['Adj Close'].autocorr()
+print("The autocorrelation of weekly returns is %4.2f" %(autocorrelation))
+
+
+# ## Are Interest Rates Autocorrelated?
+# When you look at daily changes in interest rates, the autocorrelation is close to zero. However, if you resample the data and look at annual changes, the autocorrelation is negative. This implies that while short term changes in interest rates may be uncorrelated, long term changes in interest rates are negatively autocorrelated. A daily move up or down in interest rates is unlikely to tell you anything about interest rates tomorrow, but a move in interest rates over a year can tell you something about where interest rates are going over the next year. And this makes some economic sense: over long horizons, when interest rates go up, the economy tends to slow down, which consequently causes interest rates to fall, and vice versa.
+# 
+# The DataFrame daily_rates contains daily data of 10-year interest rates from 1962 to 2017.
+
+# ### init
+
+# In[50]:
+
+
+###################
+##### Dataframe
+###################
+
+#upload and download
+
+from downloadfromFileIO import saveFromFileIO
+""" à executer sur datacamp: (apres copie du code uploadfromdatacamp.py)
+uploadToFileIO(daily_rates)
+"""
+
+tobedownloaded="""
+{pandas.core.frame.DataFrame: {'daily_rates.csv': 'https://file.io/hZ6wSWQb'}}
+"""
+prefixToc='4.2'
+prefix = saveFromFileIO(tobedownloaded, prefixToc=prefixToc, proxy="")
+
+#initialisation
+
+import pandas as pd
+daily_rates = pd.read_csv(prefix+'daily_rates.csv',index_col=0)
+daily_rates.index = pd.to_datetime(daily_rates.index)
+
+
+# ### code
+
+# In[51]:
+
+
+# Compute the daily change in interest rates 
+daily_diff = daily_rates.pct_change()
+
+# Compute and print the autocorrelation of daily changes
+autocorrelation_daily = daily_diff['US10Y'].autocorr()
+print("The autocorrelation of daily interest rate changes is %4.2f" %(autocorrelation_daily))
+
+# Convert the daily data to annual data
+yearly_rates = daily_rates.resample(rule='A').last()
+
+# Repeat above for annual data
+yearly_diff = yearly_rates.pct_change()
+autocorrelation_yearly = yearly_diff['US10Y'].autocorr()
+print("The autocorrelation of annual interest rate changes is %4.2f" %(autocorrelation_yearly))
+
+
+# In[ ]:
+
+
+
+
