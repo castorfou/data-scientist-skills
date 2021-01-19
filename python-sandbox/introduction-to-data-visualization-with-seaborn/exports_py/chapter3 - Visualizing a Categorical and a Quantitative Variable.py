7dd9dc03389cefa9ff0581b3365585ebe629f865
castorfou <guillaume.ramelet@gmail.com>
2021-01-19 10:10:37 +0100

    seaborn start

---
+++ python-sandbox/introduction-to-data-visualization-with-seaborn/exports_py/chapter3 - Visualizing a Categorical and a Quantitative Variable.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Count plots and bar plots
+# 
+# ```python
+# 
+# ```
+
+# In[ ]:
+
+
+
+
---
+++ python-sandbox/introduction-to-data-visualization-with-seaborn/start_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate datacamp
+jupyter notebook 
