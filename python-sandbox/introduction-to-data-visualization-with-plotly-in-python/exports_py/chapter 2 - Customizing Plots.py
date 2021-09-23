df941db5e6dd729665ac18ba076af5fc07846f91
castorfou <guillaume.ramelet@gmail.com>
2021-09-23 11:54:14 +0200

    start of chap 2

---
+++ python-sandbox/introduction-to-data-visualization-with-plotly-in-python/exports_py/chapter 2 - Customizing Plots.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Bivariate visualizations
+
+# ```python
+# # scatterplot with plotly.express
+# 
+# import plotly.express as px
+# fig = px.scatter(
+#     data_frame=penguins,
+#     x="Body Mass (g)",
+#     y="Flipper Length (mm)")
+# fig.show()
+# ```
+# 
+# ![image.png](attachment:image.png)
+
+# ```python
+# # Line charts in plotly.express
+# 
+# fig = px.line(
+# data_frame=msft_stock,
+#     x='Date',
+#     y='Open',
+#     title='MSFT Stock Price (5Y)')
+# fig.show()
+# ```
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+
+
